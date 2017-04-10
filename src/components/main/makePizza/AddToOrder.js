7938450daad286c997758d261b8bc39519c32fe2
Bryan Jackson <bjack13@gmail.com>
2017-04-10 12:24:47 -0600

    removed console.logs

--- src/components/main/makePizza/AddToOrder.js
+++ src/components/main/makePizza/AddToOrder.js
@@ -44,7 +44,6 @@ class AddToOrder extends Component {
                   "crust": this.props.pizzaCrust[2],
                   "toppings": this.props.customToppingArr
                 })
-
               }}>
 
               <View>
